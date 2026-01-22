791ba478d80b8ff59e7af7f99e41953fc2b837b6
Jacob Cahoon <booka66@Jacobs-MacBook-Pro-3.local>
2026-01-21 19:36:32 -0700

    spectre!

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -2,3 +2,12 @@ add("nvim-pack/nvim-spectre")
 add("nvim-lua/plenary.nvim")
 
 require("spectre").setup()
+
+local utils = require("utils")
+
+vim.keymap.set("n", "<leader>rr", function()
+	require("spectre").open({ is_insert_mode = true })
+	if utils.is_window_vertical() then
+		vim.cmd("wincmd J") -- Move spectre window to bottom
+	end
+end, { desc = "Open Spectre (adaptive layout)" })
